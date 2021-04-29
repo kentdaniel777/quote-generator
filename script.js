c7c523694c2fe3b860cff91a92d52bce1414bd3b
Fish-A-code <kentdaniel777@yahoo.com>
2021-04-29 18:24:59 +0700

    cleaner code , added tweet functionality

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn= document.getElementById("new-quote");
+const twitterBtn = document.getElementById("twitter");
+
 // get quote from API
 async function getQuote(){
     
     const proxyUrl = 'https://guarded-peak-77488.herokuapp.com/'// to enable CORS
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
     try {
-        const response = await fetch(proxyUrl + apiUrl)
-        const data = await response.json()
-        console.log(data);
-        return data;
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+
+        // use "Unknown" if there's no author
+        if (data.quoteAuthor===""){
+            authorText.innerText="Unknown"
+        }else{
+            authorText.innerText=data.quoteAuthor;
+        }
+
+        // reduce font size for long quotes
+        if (data.quoteText.length > 125){
+            quoteText.classList.add(".long-quote");
+        }else{
+            quoteText.classList.remove(".long-quote");
+        }
+        quoteText.innerText=data.quoteText;
+        
     }catch(err){
         // if there's error in loading the data , try again
         getQuote();
-        console.log("No quote :(",err);
+        
     }
 }
 
-async function Update(){
-    const quote = await getQuote();// wait until it is finished
-    // console.log(quote)
-    const txt = document.querySelector("#quote");
-    const author = document.querySelector("#author")
-    txt.innerHTML=quote["quoteText"];
-    if (quote["quoteAuthor"]!=""){
-        author.innerHTML=quote["quoteAuthor"];
-    }else{
-        author.innerHTML="Unknown";
-    }
-    
+// tweet function
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,"_blank"); // opens twitter in new window
 }
-const newQuote= document.querySelector("#new-quote");
-newQuote.addEventListener("click", Update);
+
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click",tweetQuote);
